3428fe194d419418982f2996db7e3f7ee478e859
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-09 17:21:00 +0900

    TestPlayerを作成

---
+++ CocktailKir/Assets/Test/TestPlayer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AkiVACO;
+
+public class TestPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private float m_moveSpeed = 5.0f;
+
+    private CharacterController m_ctrl;
+
+    void Awake()
+    {
+        m_ctrl = this.GetComponent<CharacterController>();
+        XLogger.LogValidObject(m_ctrl == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("CharacterController"), gameObject);
+
+
+    }
+
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Vector3 vecMove = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            vecMove.z = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vecMove.z = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vecMove.x = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vecMove.x = 1.0f;
+        }
+        vecMove.y = Physics.gravity.y;
+
+        vecMove = vecMove.normalized * (m_moveSpeed * Time.deltaTime);
+        vecMove = this.transform.TransformDirection(vecMove);
+        m_ctrl.Move(vecMove);
+        
+    }
+}
