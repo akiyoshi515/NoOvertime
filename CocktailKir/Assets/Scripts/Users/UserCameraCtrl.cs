b62f2f4e3661137b7d097896ea8ce139364abba3
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-09 17:21:52 +0900

    リグとピボットによりユーザーを追跡するユーザーカメラを作成 回転制御を仮作成

---
+++ CocktailKir/Assets/Scripts/Users/UserCameraCtrl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AkiVACO;
+
+public class UserCameraCtrl : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_pivot = null;
+
+    [SerializeField]
+    private Transform m_target = null;
+
+    [SerializeField]
+    private float m_rotateSpeed = 20.0f;
+
+    void Awake()
+    {
+        XLogger.LogValidObject(m_pivot == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Pivot"), gameObject);
+        XLogger.LogValidObject(m_target == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Target"), gameObject);
+
+    }
+	
+    void Update()
+    {
+        float delta = Time.deltaTime;
+        float angle = 0.0f;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angle -= m_rotateSpeed * delta;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            angle += m_rotateSpeed * delta;
+        }
+
+        UpdatePosition();
+        if (angle != 0.0f)
+        {
+            m_pivot.Rotate(Vector3.up, angle);
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        m_pivot.position = m_target.position;
+    }
+
+}
