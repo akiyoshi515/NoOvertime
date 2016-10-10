7497be42700873614bd9a7c7fccaf9195348d68b
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-10 18:04:10 +0900

    UserCameraの自動回り込み番を追加

---
+++ CocktailKir/Assets/Scripts/Users/UserCameraAutoCtrl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AkiVACO;
+
+public class UserCameraAutoCtrl : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_pivot = null;
+
+    [SerializeField]
+    private Transform m_target = null;
+
+    [SerializeField]
+    private float m_rotateSpeed = 10.0f;
+
+    void Awake()
+    {
+        XLogger.LogValidObject(m_pivot == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Pivot"), gameObject);
+        XLogger.LogValidObject(m_target == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Target"), gameObject);
+
+    }
+	
+    void Update()
+    {
+        m_pivot.rotation = Quaternion.Slerp(m_pivot.rotation, m_target.rotation, m_rotateSpeed * Time.deltaTime);
+        m_pivot.position = m_target.position;
+    }
+
+}
