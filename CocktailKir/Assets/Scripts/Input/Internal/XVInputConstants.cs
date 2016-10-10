d7b8393363fc88b936b9211bfc24aef432252c45
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-10 21:11:07 +0900

    XVInputの共通Constantsを作成 InputManager関連のconst文字列を追加

---
+++ CocktailKir/Assets/Scripts/Input/Internal/XVInputConstants.cs
@@ -0,0 +1,24 @@
+﻿
+namespace XVInputInternal
+{
+    public static class XVInputConstants
+    {
+        // DPad
+        public static string DPadAxisX = "DPadAxisX_";
+        public static string DPadAxisY = "DPadAxisY_";
+
+        // Left Axis
+        public static string LAxisX = "LAxisX_";
+        public static string LAxisY = "LAxisY_";
+
+        // Right Axis
+        public static string RAxisX = "RAxisX_";
+        public static string RAxisY = "RAxisY_";
+
+        // Trigger
+        public static string LTrigger = "LTrigger_";
+        public static string RTrigger = "RTrigger_";
+        
+    }
+
+}   // End of namespace XBoxInput
