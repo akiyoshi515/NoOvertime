077401eaf20b30d950c55a9f49a6431aa4e03dfc
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-10 22:10:14 +0900

    InputManagerをMenuBarから再設定できるスクリプトを作成 InputManagerをXBGamePad向けに更新

---
+++ CocktailKir/Assets/Editor/MenuInputManager/InputManagerGenerator.cs
@@ -0,0 +1,174 @@
+﻿
+// 引用元：http://notargs.com/blog/blog/2015/01/23/92/
+
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace NotargsInputManagerUtil
+{
+    public enum AxisType
+    {
+        KeyOrMouseButton = 0,
+        MouseMovement = 1,
+        JoystickAxis = 2
+    };
+
+    /// <summary>
+    /// 入力の情報
+    /// </summary>
+    public class InputAxis
+    {
+        public string name = "";
+        public string descriptiveName = "";
+        public string descriptiveNegativeName = "";
+        public string negativeButton = "";
+        public string positiveButton = "";
+        public string altNegativeButton = "";
+        public string altPositiveButton = "";
+
+        public float gravity = 0;
+        public float dead = 0;
+        public float sensitivity = 0;
+
+        public bool snap = false;
+        public bool invert = false;
+
+        public AxisType type = AxisType.KeyOrMouseButton;
+
+        // 1から始まる。
+        public int axis = 1;
+        // 0なら全てのゲームパッドから取得される。1以降なら対応したゲームパッド。
+        public int joyNum = 0;
+
+        /// <summary>
+        /// 押すと1になるキーの設定データを作成する
+        /// </summary>
+        public static InputAxis CreateButton(string name, string positiveButton, string altPositiveButton)
+        {
+            var axis = new InputAxis();
+            axis.name = name;
+            axis.positiveButton = positiveButton;
+            axis.altPositiveButton = altPositiveButton;
+            axis.gravity = 1000;
+            axis.dead = 0.001f;
+            axis.sensitivity = 1000;
+            axis.type = AxisType.KeyOrMouseButton;
+
+            return axis;
+        }
+
+        /// <summary>
+        /// ゲームパッド用の軸の設定データを作成する
+        /// </summary>
+        public static InputAxis CreatePadAxis(string name, int joystickNum, int axisNum)
+        {
+            var axis = new InputAxis();
+            axis.name = name;
+            axis.dead = 0.2f;
+            axis.sensitivity = 1;
+            axis.type = AxisType.JoystickAxis;
+            axis.axis = axisNum;
+            axis.joyNum = joystickNum;
+
+            return axis;
+        }
+
+        /// <summary>
+        /// キーボード用の軸の設定データを作成する
+        /// </summary>
+        public static InputAxis CreateKeyAxis(string name, string negativeButton, string positiveButton, string altNegativeButton, string altPositiveButton)
+        {
+            var axis = new InputAxis();
+            axis.name = name;
+            axis.negativeButton = negativeButton;
+            axis.positiveButton = positiveButton;
+            axis.altNegativeButton = altNegativeButton;
+            axis.altPositiveButton = altPositiveButton;
+            axis.gravity = 3;
+            axis.sensitivity = 3;
+            axis.dead = 0.001f;
+            axis.type = AxisType.KeyOrMouseButton;
+
+            return axis;
+        }
+    }
+
+    /// <summary>
+    /// InputManager生成器
+    /// </summary>
+    public class InputManagerGenerator
+    {
+        protected SerializedObject m_serializedObject = null;
+        protected SerializedProperty m_axesProperty = null;
+
+        public InputManagerGenerator()
+        {
+            Object[] table = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/InputManager.asset");
+            m_serializedObject = new SerializedObject(table[0]);
+            m_axesProperty = m_serializedObject.FindProperty("m_Axes");
+        }
+
+        public void AddAxis(InputAxis axis)
+        {
+            if (axis.axis < 1)
+            {
+                Debug.LogError("Invalid argument value : axis < 1");
+            }
+
+            SerializedProperty axesProperty = m_serializedObject.FindProperty("m_Axes");
+
+            axesProperty.arraySize++;
+            m_serializedObject.ApplyModifiedProperties();
+
+            SerializedProperty axisProperty = axesProperty.GetArrayElementAtIndex(axesProperty.arraySize - 1);
+
+            GetChildProperty(axisProperty, "m_Name").stringValue = axis.name;
+            GetChildProperty(axisProperty, "descriptiveName").stringValue = axis.descriptiveName;
+            GetChildProperty(axisProperty, "descriptiveNegativeName").stringValue = axis.descriptiveNegativeName;
+            GetChildProperty(axisProperty, "negativeButton").stringValue = axis.negativeButton;
+            GetChildProperty(axisProperty, "positiveButton").stringValue = axis.positiveButton;
+            GetChildProperty(axisProperty, "altNegativeButton").stringValue = axis.altNegativeButton;
+            GetChildProperty(axisProperty, "altPositiveButton").stringValue = axis.altPositiveButton;
+            GetChildProperty(axisProperty, "gravity").floatValue = axis.gravity;
+            GetChildProperty(axisProperty, "dead").floatValue = axis.dead;
+            GetChildProperty(axisProperty, "sensitivity").floatValue = axis.sensitivity;
+            GetChildProperty(axisProperty, "snap").boolValue = axis.snap;
+            GetChildProperty(axisProperty, "invert").boolValue = axis.invert;
+            GetChildProperty(axisProperty, "type").intValue = (int)axis.type;
+            GetChildProperty(axisProperty, "axis").intValue = axis.axis - 1;
+            GetChildProperty(axisProperty, "joyNum").intValue = axis.joyNum;
+
+            m_serializedObject.ApplyModifiedProperties();
+
+        }
+
+        public void Clear()
+        {
+            m_axesProperty.ClearArray();
+            m_serializedObject.ApplyModifiedProperties();
+        }
+
+        private SerializedProperty GetChildProperty(SerializedProperty parent, string name)
+        {
+            SerializedProperty child = parent.Copy();
+            child.Next(true);
+            do
+            {
+                if (child.name == name)
+                {
+                    return child;
+                }
+            }
+            while (child.Next(false));
+
+            return null;
+        }
+
+    }
+
+}
+
+#endif
---
+++ CocktailKir/Assets/Editor/MenuInputManager/InputManagerSetter.cs
@@ -0,0 +1,205 @@
+﻿
+// 引用元：http://notargs.com/blog/blog/2015/01/23/92/
+
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using XVInputInternal;
+
+namespace NotargsInputManagerUtil
+{
+    /// <summary>
+    /// InputManagerを自動的に設定してくれるクラス
+    /// </summary>
+    public class InputManagerSetter
+    {
+        /// <summary>
+        /// インプットマネージャーを再設定します。
+        /// </summary>
+        [MenuItem("Utility/Reset InputManager for XBox")]
+        public static void ResetInputManager()
+        {
+            Debug.Log("インプットマネージャーの設定を開始します。");
+            InputManagerGenerator inputManagerGenerator = new InputManagerGenerator();
+
+            Debug.Log("設定を全てクリアします。");
+            inputManagerGenerator.Clear();
+
+            Debug.Log("プレイヤーごとの設定を追加します。");
+            for (int i = 0; i < 4; i++)
+            {
+                AddPlayerInputSettingsForXBox(inputManagerGenerator, i);
+            }
+
+            Debug.Log("グローバル設定を追加します。");
+            AddGlobalInputSettings(inputManagerGenerator);
+
+            Debug.Log("スタンダードな設定を追加します。");
+            AddStdInputSettings(inputManagerGenerator);
+
+            Debug.Log("インプットマネージャーの設定が完了しました。");
+        }
+
+        /// <summary>
+        /// グローバルな入力設定を追加する（OK、キャンセルなど）
+        /// </summary>
+        /// <param name="inputManagerGenerator">Input manager generator.</param>
+        private static void AddGlobalInputSettings(InputManagerGenerator inputManagerGenerator)
+        {
+            /*
+            // 横方向
+            {
+                var name = "Horizontal";
+                inputManagerGenerator.AddAxis(InputAxis.CreatePadAxis(name, 0, 1));
+                inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "a", "d", "left", "right"));
+            }
+            */
+        }
+
+        /// <summary>
+        /// プレイヤーごとの入力設定を追加する
+        /// </summary>
+        /// <param name="inputManagerGenerator">Input manager generator.</param>
+        /// <param name="playerIndex">Player index.</param>
+        private static void AddPlayerInputSettingsForXBox(InputManagerGenerator inputManagerGenerator, int playerIndex)
+        {
+            if (playerIndex < 0 || playerIndex > 3)
+            {
+                Debug.LogError("プレイヤーインデックスの値が不正です。");
+            }
+
+            int joystickNum = playerIndex + 1;
+
+            string strIndex = playerIndex.ToString();
+
+            // 左スティック
+            {
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.LAxisX + strIndex, joystickNum, 1));
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.LAxisY + strIndex, joystickNum, 2));
+            }
+
+            // 右スティック
+            {
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.RAxisX + strIndex, joystickNum, 4));
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.RAxisY + strIndex, joystickNum, 5));
+            }
+
+            // 十字キー
+            {
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.DPadAxisX + strIndex, joystickNum, 6));
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.DPadAxisY + strIndex, joystickNum, 7));
+            }
+
+            // トリガー
+            {
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.LTrigger + strIndex, joystickNum, 9));
+                inputManagerGenerator.AddAxis(
+                    InputAxis.CreatePadAxis(XVInputConstants.RTrigger + strIndex, joystickNum, 10));
+            }
+
+        }
+
+        /// <summary>
+        /// デフォルトのEventSystemで扱う入力設定を追加する
+        /// </summary>
+        /// <param name="inputManagerGenerator">Input manager generator.</param>
+        private static void AddStdInputSettings(InputManagerGenerator inputManagerGenerator)
+        {
+            // 横方向
+            {
+                var name = "Horizontal";
+                inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "a", "d", "left", "right"));
+            }
+
+            // 縦方向
+            {
+                var name = "Vertical";
+                inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "s", "w", "down", "up"));
+            }
+
+            // 決定
+            {
+                var name = "Submit";
+                inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "", "enter", "", "space"));
+            }
+
+            // キャンセル
+            {
+                var name = "Cancel";
+                inputManagerGenerator.AddAxis(InputAxis.CreateKeyAxis(name, "", "escape", "", "joystick button 1"));
+            }
+
+        }
+
+        /// <summary>
+        /// キーボードでプレイした場合、割り当たっているキーを取得する
+        /// </summary>
+        /// <param name="upKey">Up key.</param>
+        /// <param name="downKey">Down key.</param>
+        /// <param name="leftKey">Left key.</param>
+        /// <param name="rightKey">Right key.</param>
+        /// <param name="attackKey">Attack key.</param>
+        /// <param name="playerIndex">Player index.</param>
+        private static void GetAxisKey(out string upKey, out string downKey, out string leftKey, out string rightKey, out string attackKey, int playerIndex)
+        {
+            upKey = "";
+            downKey = "";
+            leftKey = "";
+            rightKey = "";
+            attackKey = "";
+
+            switch (playerIndex)
+            {
+                case 0:
+                    upKey = "w";
+                    downKey = "s";
+                    leftKey = "a";
+                    rightKey = "d";
+                    attackKey = "e";
+                    break;
+                case 1:
+                    upKey = "i";
+                    downKey = "k";
+                    leftKey = "j";
+                    rightKey = "l";
+                    attackKey = "o";
+                    break;
+                case 2:
+                    upKey = "up";
+                    downKey = "down";
+                    leftKey = "left";
+                    rightKey = "right";
+                    attackKey = "[0]";
+                    break;
+                case 3:
+                    upKey = "[8]";
+                    downKey = "[5]";
+                    leftKey = "[4]";
+                    rightKey = "[6]";
+                    attackKey = "[9]";
+                    break;
+                default:
+                    Debug.LogError("プレイヤーインデックスの値が不正です。");
+                    upKey = "";
+                    downKey = "";
+                    leftKey = "";
+                    rightKey = "";
+                    attackKey = "";
+                    break;
+            }
+        }
+    }
+
+}
+
+#endif
