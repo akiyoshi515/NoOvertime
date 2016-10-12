9e3e02608c1f7f1b2603aea84b958f9da5860339
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-12 15:32:00 +0900

    スコアの計算Unitを作成

---
+++ CocktailKir/Assets/Scripts/Guest/GuestScoreUnit.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuestScoreUnit
+{
+    private int[] m_scoreTable = new int[4];
+    private int m_topUserId = -1;   // -1 = None
+
+    public int topUserId
+    {
+        get { return m_topUserId; }
+    }
+
+    public GuestScoreUnit()
+    {
+        Reset();
+        GuestScores.AddMax();
+    }
+
+    /// <summary>
+    /// スコア等のリセット
+    /// </summary>
+    public void Reset()
+    {
+        m_scoreTable[0] = 0;
+        m_scoreTable[1] = 0;
+        m_scoreTable[2] = 0;
+        m_scoreTable[3] = 0;
+        m_topUserId = -1;
+    }
+
+    /// <summary>
+    /// スコア加算
+    /// </summary>
+    /// <param name="id">ユーザーID</param>
+    /// <param name="s">加算スコア</param>
+    /// <returns>スコアトップが更新されたか？</returns>
+    public bool Add(UserID id, int s)
+    {
+        int idx = (int)id;
+        m_scoreTable[idx] += s;
+        if (m_topUserId == -1)
+        {
+            GuestScores.Add(id);
+            m_topUserId = idx;
+            return true;
+        }
+        else
+        {
+            // HACK 後から来たUserと一位タイの場合は前のUserが優先
+            if (m_scoreTable[idx] > m_scoreTable[m_topUserId])
+            {
+                GuestScores.Add(id, (UserID)m_topUserId);
+                m_topUserId = idx;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ CocktailKir/Assets/Scripts/Guest/GuestScores.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+public class GuestScores 
+{
+    private static int[] m_scoreTable = new int[4]{0, 0, 0, 0};
+    private static int m_maxScore = 0;
+    public static int maxScore
+    {
+        get { return m_maxScore; }
+    }
+
+    public static void Reset()
+    {
+        m_scoreTable[0] = 0;
+        m_scoreTable[1] = 0;
+        m_scoreTable[2] = 0;
+        m_scoreTable[3] = 0;
+        m_maxScore = 0;
+    }
+
+    public static void Add(UserID id)
+    {
+        int idx = (int)id;
+        m_scoreTable[idx] += 1;
+    }
+
+    public static void Add(UserID id, UserID prevId)
+    {
+        int idx = (int)id;
+        int prev = (int)prevId;
+        m_scoreTable[idx] += 1;
+        m_scoreTable[prev] -= 1;
+    }
+
+    public static void AddMax()
+    {
+        ++m_maxScore;
+    }
+
+    public static int GetScore(UserID id)
+    {
+        int idx = (int)id;
+        return m_scoreTable[idx];
+    }
+
+}
