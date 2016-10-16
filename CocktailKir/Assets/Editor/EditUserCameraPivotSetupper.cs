2922216b55cdaffd3ac756e16cd391de965de833
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-17 03:17:23 +0900

    UserCameraのPivotに対して、セットアッパーを作成 通常・構え両方のPivotを設定できるように変更

---
+++ CocktailKir/Assets/Editor/EditUserCameraPivotSetupper.cs
@@ -0,0 +1,78 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using AkiVACO;
+using AkiVACO.XValue;
+
+#if DEBUG
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+[CustomEditor(typeof(UserCameraPivotSetupper))]
+public class EditUserCameraPivotSetupper : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        UserCameraPivotSetupper gen = target as UserCameraPivotSetupper;
+
+        EditorGUILayout.LabelField("初期カメラ");
+        gen.m_stdPivot.position = EditorGUILayout.Vector3Field("座標", gen.m_stdPivot.position);
+        gen.m_stdPivot.rotation = ToQuaternion(EditorGUILayout.Vector4Field("回転", ToVector4(gen.m_stdPivot.rotation)));
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("構えカメラ");
+        gen.m_shotPivot.position = EditorGUILayout.Vector3Field("座標", gen.m_shotPivot.position);
+        gen.m_shotPivot.rotation = ToQuaternion(EditorGUILayout.Vector4Field("回転", ToVector4(gen.m_shotPivot.rotation)));
+        EditorGUILayout.Space();
+
+        EditorGUILayout.HelpBox("現在のカメラ位置に設定します", MessageType.Info);
+        if (GUILayout.Button("Set 初期カメラ位置"))
+        {
+            Transform camera = gen.transform.GetChild(0);
+            camera.localPosition = gen.m_stdPivot.position;
+            camera.localRotation = gen.m_stdPivot.rotation;
+        }
+
+        if (GUILayout.Button("Set 構えカメラ位置"))
+        {
+            Transform camera = gen.transform.GetChild(0);
+            camera.localPosition = gen.m_shotPivot.position;
+            camera.localRotation = gen.m_shotPivot.rotation;
+        }
+
+        EditorGUILayout.HelpBox("現在のカメラ位置を保存します", MessageType.Info);
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Setup 初期カメラ位置"))
+        {
+            Transform camera = gen.transform.GetChild(0);
+            gen.m_stdPivot.position = camera.localPosition;
+            gen.m_stdPivot.rotation = camera.localRotation;
+        }
+
+        if (GUILayout.Button("Setup 構えカメラ位置"))
+        {
+            Transform camera = gen.transform.GetChild(0);
+            gen.m_shotPivot.position = camera.localPosition;
+            gen.m_shotPivot.rotation = camera.localRotation;
+        }
+    }
+
+    public Quaternion ToQuaternion(Vector4 v)
+    {
+        return new Quaternion(v.x, v.y, v.z, v.w);
+    }
+
+    public Vector4 ToVector4(Quaternion v)
+    {
+        return new Vector4(v.x, v.y, v.z, v.w);
+    }
+}
+
+#endif
+
+#endif
---
+++ CocktailKir/Assets/Scripts/Users/UserCameraPivotSetupper.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserCameraPivotSetupper : MonoBehaviour
+{
+    [System.Serializable]
+    public class Point
+    {
+        public Quaternion rotation = Quaternion.identity;
+        public Vector3 position = Vector3.zero;
+    }
+
+    public Point m_stdPivot = new Point();
+    public Point m_shotPivot = new Point();
+
+    private Transform m_camera = null;
+
+    private float m_lerpRate = 1.0f;
+    private float m_time = 0.0f;
+    private float m_rate = 0.0f;    // 1.0f or -1.0f or 0.0f
+    private bool m_prevLauncherStance = false;
+
+    void Start()
+    {
+        m_camera = this.transform.GetChild(0);
+        m_camera.localPosition = m_stdPivot.position;
+        m_camera.localRotation = m_stdPivot.rotation;
+        m_rate = 0.0f;
+    }
+
+    void Update()
+    {
+        IXVInput input = XVInput.GetInterface(UserID.User1);
+        bool bl = input.IsLauncherStance();
+        if (m_prevLauncherStance ^ bl)
+        {
+            if (bl)
+            {
+                // Start
+                m_rate = 1.0f;
+            }
+            else
+            {
+                // Stop
+                m_rate = -1.0f;
+            }
+            m_prevLauncherStance = bl;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (m_rate == 0.0f)
+        {
+            return;
+        }
+
+        m_time += m_rate * (m_lerpRate * Time.deltaTime);
+        if (m_time >= 1.0f)
+        {
+            m_time = 1.0f;
+            m_rate = 0.0f;
+        }
+        else if (m_time <= 0.0f)
+        {
+            m_time = 0.0f;
+            m_rate = 0.0f;
+        }
+
+        m_camera.localPosition = Vector3.Lerp(m_stdPivot.position, m_shotPivot.position, m_time);
+        m_camera.localRotation = Quaternion.Slerp(m_stdPivot.rotation, m_shotPivot.rotation, m_time);
+    }
+
+    public void SetLerpTime(float time)
+    {
+        m_lerpRate = 1.0f / time;
+    }
+
+    public void SetStdPivot()
+    {
+        m_rate = -1.0f;
+    }
+
+    public void SetShotPivot()
+    {
+        m_rate = 1.0f;
+    }
+}
