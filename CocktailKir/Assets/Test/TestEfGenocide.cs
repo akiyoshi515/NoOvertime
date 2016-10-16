6895f3c0ea44852c4a1b8f9c04dd40c27ddff70d
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-17 05:17:31 +0900

    TestEffect　ジェノザイドを作成

---
+++ CocktailKir/Assets/Test/TestEfGenocide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEfGenocide : MonoBehaviour
+{
+    private float m_lifeTime = 10.0f;
+    private ParticleSystem m_particle = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_particle = this.transform.GetChild(0).GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_lifeTime -= Time.deltaTime;
+        if (m_lifeTime <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
