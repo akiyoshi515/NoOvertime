909c2057609180244466f27f9238f3faf9c91cc6
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-17 05:19:26 +0900

    着弾時に登録しているObjectがあれば生成するように変更

--- CocktailKir/Assets/Test/TestBalletCtrl.cs
+++ CocktailKir/Assets/Test/TestBalletCtrl.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class TestBalletCtrl : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject m_onDestroyedObject = null;
+
     void OnCollisionEnter(Collision col)
     {
         if (col.collider.tag != "Ballet")
         {
+            if (m_onDestroyedObject != null)
+            {
+                GameObject.Instantiate(m_onDestroyedObject, this.transform.position, Quaternion.identity);
+            }
             Destroy(this.gameObject);
         }
     }
