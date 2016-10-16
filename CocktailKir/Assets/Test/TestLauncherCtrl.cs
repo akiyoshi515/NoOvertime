9ec3805a802b2274016473af7a7a851995513eb5
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-17 05:20:17 +0900

    Testランチャーにチャージショットを追加 チャージ弾を設定

--- CocktailKir/Assets/Test/TestLauncherCtrl.cs
+++ CocktailKir/Assets/Test/TestLauncherCtrl.cs
@@ -26,6 +26,9 @@ public class TestLauncherCtrl : MonoBehaviour {
     private float m_knockbackTime = 0.250f;
 
     [SerializeField]
+    private float m_chargeShotTime = 1.0f;
+
+    [SerializeField]
     private float m_hitPointOffset = 1.0f;
 
     [SerializeField]
@@ -38,20 +41,34 @@ public class TestLauncherCtrl : MonoBehaviour {
     private GameObject m_ballet = null;
 
     [SerializeField]
+    private GameObject m_balletBouquet = null;
+
+    [SerializeField]
     private GameObject m_hitPoint = null;
 
+    [SerializeField]
+    private GameObject m_efMaxCharge = null;
+
     private LineRenderer m_lineRenderer = null;
+    private IEffect m_csEfMaxCharge = null;
 
     private float m_knockback = 0.0f;
+    private float m_chargeTime = 0.0f;
 
     void Awake()
     {
         XLogger.LogValidObject(m_parent == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Parent"), gameObject);
         XLogger.LogValidObject(m_ballet == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Ballte"), gameObject);
+        XLogger.LogValidObject(m_balletBouquet == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("BallteBouquet"), gameObject);
         XLogger.LogValidObject(m_hitPoint == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("HitPoint"), gameObject);
 
+        XLogger.LogValidObject(m_efMaxCharge == null, LibConstants.ErrorMsg.GetMsgNotBoundComponent("Effect MaxCharge"), gameObject);
+        
         m_lineRenderer = this.GetComponent<LineRenderer>();
 
+        GameObject efObj = XFunctions.InstanceChild(m_efMaxCharge, Vector3.zero, Quaternion.identity, this.gameObject);
+
+        m_csEfMaxCharge = efObj.GetComponent<IEffect>();
     }
 
 	// Use this for initialization
@@ -68,10 +85,7 @@ public class TestLauncherCtrl : MonoBehaviour {
 
         UpdateKnockbackTime();
 
-        if (input.IsShot())
-        {
-            LaunchBallet();
-        }
+        UpdateShotBallet(input.IsShot());
 
         Vector2 vec = input.RotateLauncher();
 
@@ -104,17 +118,61 @@ public class TestLauncherCtrl : MonoBehaviour {
         }
     }
 
-    void LaunchBallet()
+    void UpdateShotBallet(bool isShot)
     {
         if (m_knockback > 0.0f)
         {
             return;
         }
 
+        if (isShot)
+        {
+            m_chargeTime += Time.deltaTime;
+        }
+        else
+        {
+            if (m_chargeTime > 0.0f)
+            {
+                if (m_chargeTime < m_chargeShotTime)
+                {
+                    LaunchBallet();
+                }
+                else
+                {
+                    LaunchBouquet();
+                }
+                m_chargeTime = 0.0f;
+            }
+        }
+
+        if (m_chargeTime >= m_chargeShotTime)
+        {
+            m_csEfMaxCharge.WakeupEffect();
+        }
+        else
+        {
+            m_csEfMaxCharge.SleepEffect();
+        }
+    }
+
+    void LaunchBallet()
+    {
         Quaternion rot = this.gameObject.transform.rotation;
         Vector3 pos = this.transform.position + (rot * m_launchPoint);
 
         GameObject obj = XFunctions.Instance(m_ballet, pos, rot);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.AddForce(this.transform.forward * m_shotPower, ForceMode.Impulse);
+
+        m_knockback += m_knockbackTime;
+    }
+
+    void LaunchBouquet()
+    {
+        Quaternion rot = this.gameObject.transform.rotation;
+        Vector3 pos = this.transform.position + (rot * m_launchPoint);
+
+        GameObject obj = XFunctions.Instance(m_balletBouquet, pos, rot);
         Rigidbody rb = obj.GetComponent<Rigidbody>();
         rb.AddForce(this.transform.forward * m_shotPower, ForceMode.Impulse);
 
