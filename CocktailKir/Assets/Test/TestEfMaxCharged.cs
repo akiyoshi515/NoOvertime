7798060ba4eadfd3e384bc72dbfbc9d52c3892a2
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-17 05:18:18 +0900

    TestEffect　チャージ時エフェクトを作成

---
+++ CocktailKir/Assets/Test/TestEfMaxCharged.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEfMaxCharged : MonoBehaviour, IEffect
+{
+    [SerializeField]
+    private ParticleSystem m_particleCharge = null;
+
+    [SerializeField]
+    private ParticleSystem m_particleBarn = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+    }
+
+    public void WakeupEffect()
+    {
+        m_particleCharge.Play();
+    }
+
+    public void SleepEffect()
+    {
+        if (m_particleCharge.isPlaying)
+        {
+            m_particleCharge.Stop();
+            m_particleBarn.Play();
+        }
+    }
+
+}
