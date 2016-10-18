08dfd2a67484e9f879b135e1b14a99c629fb72ae
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-18 10:00:24 +0900

    弾の衝突判定用イベントコリジョンを作成

---
+++ CocktailKir/Assets/Scripts/Ballet/BalletTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalletTrigger : MonoBehaviour
+{
+    private bool m_flag = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_flag)
+        {
+            Destroy(this.gameObject);
+        }
+        m_flag = true;
+    }
+}
