88304057c315ca416a637c256dcaa29afa2b35cf
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-18 10:02:21 +0900

    各弾丸にイベントコリジョンを追加 物理コリジョンを最小化

--- CocktailKir/Assets/Test/TestBalletCtrl.cs
+++ CocktailKir/Assets/Test/TestBalletCtrl.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using AkiVACO;
+
 public class TestBalletCtrl : MonoBehaviour
 {
+    [SerializeField]
+    private float m_hitRadius = 1;
+
     [SerializeField]
     private int m_cost = 1;
     public int cost
@@ -11,25 +16,51 @@ public class TestBalletCtrl : MonoBehaviour
     }
 
     [SerializeField]
-    private GameObject m_onDestroyedObject = null;
+    private GameObject m_hitTrigger = null;
+
+    [SerializeField]
+    private GameObject[] m_onDestroyedObject = null;
 
     private bool m_isDead = false;
 
+    void Start()
+    {
+        if (m_hitTrigger != null)
+        {
+            SphereCollider col = m_hitTrigger.GetComponent<SphereCollider>();
+            col.radius = m_hitRadius;
+        }
+    }
+
     void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag != "Ballet")
+        {
+            SendHit();
+        }
+    }
+
+    public void SendHit()
     {
         if (m_isDead)
         {
             return;
         }
 
-        if (col.collider.tag != "Ballet")
+        if (m_hitTrigger != null)
         {
-            if (m_onDestroyedObject != null)
-            {
-                GameObject.Instantiate(m_onDestroyedObject, this.transform.position, Quaternion.identity);
-            }
-            m_isDead = true;
-            Destroy(this.gameObject);
+            GameObject.Instantiate(m_hitTrigger, this.transform.position, Quaternion.identity);
         }
+
+        if (m_onDestroyedObject != null)
+        {
+            foreach (GameObject obj in m_onDestroyedObject)
+            {
+                GameObject.Instantiate(obj, this.transform.position, Quaternion.identity);
+            }
+        }
+        m_isDead = true;
+        Destroy(this.gameObject);
+        
     }
 }
