3cc9dd1673c2fcb8423aeb8907c9e54888f1a08a
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-18 10:03:29 +0900

    GuestのBallet衝突判定のみ先行して実装

---
+++ CocktailKir/Assets/Scripts/Guest/GuestCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuestCtrl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Ballet")
+        {
+            col.gameObject.GetComponent<TestBalletCtrl>().SendHit();
+        }
+        else if (col.tag == "BalletTrigger")
+        {
+            AkiVACO.XLogger.Log("Hit!");
+        }
+    }
+
+}
