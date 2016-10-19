0cbb006c295cf99f3b31099bdd6abe73e96b54b7
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-19 17:53:19 +0900

    衝突時のインパルスを正反射するCSを実装

---
+++ CocktailKir/Assets/Scripts/CharCtrl/PhysicCollisionImpulseReflecter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class PhysicCollisionImpulseReflecter : MonoBehaviour
+{
+    public string[] m_targetTag = new string[1];
+
+    void OnCollisionStay(Collision col)
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        foreach(string strTag in m_targetTag){
+            if (col.gameObject.tag == strTag)
+            {
+                rb.AddForce(col.impulse, ForceMode.Impulse);
+            }
+        }
+    }
+}
