babfcc3a47b6d76aa33b192d99118337b4b41b41
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-20 17:03:43 +0900

    GuestScore（人数）のViewerを作成

---
+++ CocktailKir/Assets/Editor/EditGuestScoreViewer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if DEBUG
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+[CustomEditor(typeof(GuestScoreViewer))]
+public class EditGuestScoreViewer : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GuestScoreViewer viewer = target as GuestScoreViewer;
+
+        EditorGUILayout.IntField("最大人数", GuestScores.maxScore);
+
+        EditorGUILayout.LabelField("魅了パラメータ");
+        EditorGUILayout.IntField("User1", GuestScores.scoreTable[0]);
+        EditorGUILayout.IntField("User2", GuestScores.scoreTable[1]);
+        EditorGUILayout.IntField("User3", GuestScores.scoreTable[2]);
+        EditorGUILayout.IntField("User4", GuestScores.scoreTable[3]);
+
+    }
+}
+
+#endif
+
+#endif
