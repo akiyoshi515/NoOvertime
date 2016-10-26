b403e6e731e230e62f6d94eb3843a351cd42a81e
Hondy000 <kazu.1994.1014@gmail.com>
2016-10-26 15:23:00 +0900

    UserName.csの名前を変更

--- CocktailKir/Assets/Hondy/UserNoUI.cs
+++ CocktailKir/Assets/Hondy/UserNoUI.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UserNameUI : MonoBehaviour {
+public class UserNoUI : MonoBehaviour {
 
     ///
     /// <summary>   Userの名前.   </summary>
