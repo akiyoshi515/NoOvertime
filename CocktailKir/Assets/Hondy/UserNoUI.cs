a7026d0c8542bd37588e9af7697f021f3f6f90e2
Hondy000 <kazu.1994.1014@gmail.com>
2016-10-26 17:27:48 +0900

    UserNoプレハブを4つ分作成

--- CocktailKir/Assets/Hondy/UserNoUI.cs
+++ CocktailKir/Assets/Hondy/UserNoUI.cs
@@ -1,45 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UserNoUI : MonoBehaviour {
-
-    ///
-    /// <summary>   Userの名前.   </summary>
-    ///
-
-    [SerializeField, Header("Userの名前")]
-    string m_name;
-    public string Name
-    {
-        get
-        {
-            return m_name;
-        }
-        set
-        {
-            m_name = value;
-            m_textMesh.text = m_name;
-        }
-    }
-
-    ///
-    /// <summary>   ユーザーごとの色.   </summary>
-    ///
-    [SerializeField, Header("表示色")]
-    Color m_userColor;
-    public UnityEngine.Color UserColor
-    {
-        get
-        {
-            return m_userColor;
-        }
-        set
-        {
-            m_userColor = value;
-            m_textMesh.color = m_userColor;
-        }
-    }
-
+public class UserNoUI : MonoBehaviour
+{
+    
     ///
     /// <summary>   回転の指標にするカメラ.  </summary>
     ///
@@ -59,22 +23,29 @@ public class UserNoUI : MonoBehaviour {
     }
 
     ///
-    /// <summary>   The text mesh.  </summary>
+    /// <summary>   trueだったら画面表示,falseだったら画面非表示. </summary>
+    ///
+    [SerializeField]
+    bool m_isDisplay;
+    public bool IsDisplay
+    {
+        get { return m_isDisplay; }
+        set { m_isDisplay = value; }
+    }
+    ///
+    /// <summary>   管理するメッシュレンダラーコンポーネント.  </summary>
     ///
 
-    TextMesh m_textMesh;
+    MeshRenderer m_meshRenderer;
 
     void Start()
     {
-        m_textMesh = gameObject.GetComponent<TextMesh>();
-        m_textMesh.color = m_userColor;
-        m_textMesh.text = m_name;
+        m_meshRenderer = GetComponent<MeshRenderer>();
     }
-
-
-
+    
     void Update()
     {
+        m_meshRenderer.enabled = m_isDisplay;
         // カメラに合わせて回転
         transform.LookAt(transform.position - m_targetCamera.transform.rotation * Vector3.back, m_targetCamera.transform.rotation * Vector3.up);
 
