2bbb3012a1fd2b22b3cbc17d806677202e5e0736
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-28 05:47:25 +0900

    GuestPopのStrategy用Interfaceを作成

---
+++ CocktailKir/Assets/Scripts/Guest/Strategy/IGuestPopStrategy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IGuestPopStrategy
+{
+    void UpdatePopStrategy(GuestPopDestinationCtrl ctrl, int[] values, float[] fvalues);
+
+    GuestPopStrategy.StrategyType ToStrategyType();
+}
