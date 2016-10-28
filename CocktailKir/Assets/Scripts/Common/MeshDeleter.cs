805548fcd620c7dc1955f7a1a0c1d1bc88236a3b
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-28 10:15:01 +0900

    MeshのMeshRender等を実行時に削除するCSを作成

---
+++ CocktailKir/Assets/Scripts/Common/MeshDeleter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshDeleter : MonoBehaviour
+{
+
+    public bool m_isAwake = false;
+
+    void Awake()
+    {
+#if DEBUG
+        if (m_isAwake)
+        {
+            GameObject.Destroy(this.GetComponent<MeshRenderer>());
+            GameObject.Destroy(this.GetComponent<MeshFilter>());
+            GameObject.Destroy(this);
+        }
+#else
+        GameObject.Destroy(this.GetComponent<MeshRenderer>());
+        GameObject.Destroy(this.GetComponent<MeshFilter>());
+        GameObject.Destroy(this);
+#endif
+    }
+
+}
