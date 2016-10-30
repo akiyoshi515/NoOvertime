cdd754e8dc2805afe222b34a574012ee749facf2
Hondy000 <kazu.1994.1014@gmail.com>
2016-10-30 23:05:20 +0900

    テスト初期段階のUIや演出を追加 マガジンUIの基本処理は完了 動作確認したい場合はMeshUIMagazine.csのテストコード部分をコメントアウトを外してTestHondySightで確認してください

---
+++ CocktailKir/Assets/Hondy/Script/BulletIconEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletIconEffect
+    :
+    MonoBehaviour
+{
+
+    enum BulletState
+    {
+        NONE,
+        RELOAD_COMPLETE,
+        FILL
+    }
+
+    ///
+    /// <summary>   The state.  </summary>
+    ///
+
+    BulletState m_state;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
