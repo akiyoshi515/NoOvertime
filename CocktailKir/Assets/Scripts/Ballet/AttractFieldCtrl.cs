70d83fbff8755b6ac7cd897c4a5b21a965165f51
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-10-31 13:13:27 +0900

    集客効果　AttractFieldを作成

---
+++ CocktailKir/Assets/Scripts/Ballet/AttractFieldCtrl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AttractFieldCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private float m_radius = 1.0f;
+
+    [SerializeField]
+    private float m_time = 0.0f;
+    public float time 
+    {
+        get { return m_time; }
+        set { m_time = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<SphereCollider>().radius = m_radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time -= Time.deltaTime;
+        if (m_time <= 0.0f)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+
+
+}
+
