628fd716ba98d22ee6a4838c9aac46c9837851ba
Hondy000 <kazu.1994.1014@gmail.com>
2016-11-02 20:58:21 +0900

    DistanceUIことはじめ

---
+++ CocktailKir/Assets/Hondy/Script/DistancePartialUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+///
+/// <summary>   距離UIの部分的なもの（上下で組み合わせて表現）.  </summary>
+///
+/// <remarks>   Hondy, 2016/11/02.  </remarks>
+///
+/// <seealso cref="T:MonoBehaviour"/>
+///
+
+public class DistancePartialUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ CocktailKir/Assets/Hondy/Script/DistanceUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+///
+/// <summary>   距離UI ここで情報を受け取ってパーツを制御.  </summary>
+///
+/// <remarks>   Hondy, 2016/11/02.  </remarks>
+///
+/// <seealso cref="T:MonoBehaviour"/>
+///
+
+public class DistanceUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
