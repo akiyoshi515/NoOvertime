ab03406ad7513644be6a8149987856099ce70018
Hondy000 <kazu.1994.1014@gmail.com>
2016-11-03 15:27:44 +0900

    test版修正

--- CocktailKir/Assets/Hondy/Script/DistancePartialUI.cs
+++ CocktailKir/Assets/Hondy/Script/DistancePartialUI.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 ///
@@ -9,15 +10,49 @@ using System.Collections;
 /// <seealso cref="T:MonoBehaviour"/>
 ///
 
-public class DistancePartialUI : MonoBehaviour {
+public class DistancePartialUI : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Image m_imageCompornent;
+
+    ///
+    /// <summary>   The float icon. </summary>
+    ///
+    [SerializeField]
+    Image m_floatIcon;
+    public UnityEngine.UI.Image FloatIcon
+    {
+        get { return m_floatIcon; }
+        set { m_floatIcon = value; }
+    }
+
+    float m_startDistance;
+    float m_endDistance;
+
+    float m_currrentDistance;
+
+    Vector3 m_diffPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_imageCompornent = GetComponent<Image>();
+        m_diffPosition = m_imageCompornent.rectTransform.position;
+        m_diffPosition.y -= m_imageCompornent.rectTransform.sizeDelta.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_floatIcon)
+        {
+            m_floatIcon.rectTransform.position = new UnityEngine.Vector3(
+                m_floatIcon.rectTransform.position.x,
+                0,
+                m_floatIcon.rectTransform.position.x
+                );
+        }
+
+    }
 }
