fae3000e596fb33a72224949291b1e9db14f569d
Hondy000 <kazu.1994.1014@gmail.com>
2016-11-05 02:13:34 +0900

    レーダーと被っちゃうけどDistanceUI作成

--- CocktailKir/Assets/Hondy/Script/DistanceUI.cs
+++ CocktailKir/Assets/Hondy/Script/DistanceUI.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 ///
 /// <summary>   距離UI ここで情報を受け取ってパーツを制御.  </summary>
 ///
@@ -11,13 +11,51 @@ using System.Collections;
 
 public class DistanceUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    [SerializeField]
+    Image m_floatIcon;
+    [SerializeField]
+    float m_distanceOfCourse;
+    [SerializeField]
+    float m_currrentDistance;
+
+    Vector3 pos;
+
+    [SerializeField]
+    Canvas testCanvas;
+
+    [SerializeField]
+    NavLines nav;
+
+    [SerializeField]
+    GameObject m_float;
+
+    Vector3 m_current;
+    Vector3 m_pre;
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < nav.table.Length-1;i++)
+        {
+            m_distanceOfCourse += Vector3.Distance(nav.table[i], nav.table[i + 1]);
+        }
+        
+        Vector2 size = GetComponent<Image>().rectTransform.sizeDelta;
+        pos = new Vector3(0, -size.y * 0.5f, 0);
+        m_floatIcon.rectTransform.localPosition = pos;
+        m_current = m_float.transform.position;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        m_pre = m_current;
+        m_current = m_float.transform.position;
+
+        m_currrentDistance += Vector3.Distance(m_pre, m_current);
+
+        Vector2 size = GetComponent<Image>().rectTransform.sizeDelta;
+        pos = new Vector3(0, -size.y * 0.5f + (m_currrentDistance / m_distanceOfCourse) * size.y, 0);
+        m_floatIcon.rectTransform.localPosition = pos;
+    }
 }
