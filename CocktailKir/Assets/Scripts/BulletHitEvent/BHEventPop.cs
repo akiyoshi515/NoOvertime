9a914dd56bf6dee58fce6628d8bdddf4195ee4f9
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-11-08 23:17:19 +0900

    BulletHitのPopEvent２種を作成

---
+++ CocktailKir/Assets/Scripts/BulletHitEvent/BHEventPop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BHEventPop : BHEventBase
+{
+    [SerializeField]
+    private GameObject[] m_popObject = null;
+
+    protected override void OnHitEvent(GameObject obj)
+    {
+        foreach (GameObject popObj in m_popObject)
+        {
+            AkiVACO.XFunctions.Instance(popObj, this.transform.position, this.transform.rotation);
+        }
+    }
+}
