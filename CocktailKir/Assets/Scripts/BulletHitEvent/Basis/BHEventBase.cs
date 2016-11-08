821351a6ad717b650db50fef0fc75c34cdd1f20f
akiyoshi515 <aki.515.yosshy@gmail.com>
2016-11-08 21:17:00 +0900

    BulletのHitEventのBaseClassを作成

---
+++ CocktailKir/Assets/Scripts/BulletHitEvent/Basis/BHEventBase.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BHEventBase : MonoBehaviour
+{
+    protected abstract void OnHitEvent(GameObject obj);
+
+    [SerializeField, Header("再開までの時間")]
+    private float m_reentryTime = 1.0f;
+
+    private float m_time = 0.0f;
+    public float time
+    {
+        get { return m_time; }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (m_time <= 0.0f)
+        {
+            if ((col.gameObject.tag == "Bullet") || (col.gameObject.tag == "BulletTrigger"))
+            {
+                OnHitEvent(col.gameObject);
+                m_time = m_reentryTime;
+            }
+        }
+        else
+        {
+            m_time -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (m_time <= 0.0f)
+        {
+            if ((col.gameObject.tag == "Bullet") || (col.gameObject.tag == "BulletTrigger"))
+            {
+                OnHitEvent(col.gameObject);
+                m_time = m_reentryTime;
+            }
+        }
+        else
+        {
+            m_time -= Time.deltaTime;
+        }
+    }
+
+    public void Restart()
+    {
+        m_time = 0.0f;
+    }
+}
---
+++ CocktailKir/Assets/Scripts/BulletHitEvent/Basis/BHEventOneShotBase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BHEventOneShotBase : MonoBehaviour
+{
+    protected abstract void OnHitEvent(GameObject obj);
+
+    void OnCollisionEnter(Collision col)
+    {
+        if ((col.gameObject.tag == "Bullet") || (col.gameObject.tag == "BulletTrigger"))
+        {
+            OnHitEvent(col.gameObject);
+            this.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if ((col.gameObject.tag == "Bullet") || (col.gameObject.tag == "BulletTrigger"))
+        {
+            OnHitEvent(col.gameObject);
+            this.enabled = false;
+        }
+    }
+
+    public void Restart()
+    {
+        this.enabled = true;
+    }
+
+}
