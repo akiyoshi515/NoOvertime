ef725bcb075029af63020ccbe7cc34a3a840d0e2
Hondy000 <kazu.1994.1014@gmail.com>
2016-11-11 09:04:12 +0900

    radarmap作成Editor途中

--- CocktailKir/Assets/Editor/HondyEditor/TestRadarMeshTexture.cs
+++ CocktailKir/Assets/Editor/HondyEditor/TestRadarMeshTexture.cs
@@ -11,7 +11,11 @@ public class TestRadarMeshTexture
     Object target;
     Object test;
     RenderTexture m_rt;
-    Texture m_tex2d;
+    Texture2D[] m_tex2d;
+    RenderTexture[] _rt;
+    Vector3 m_mapCenter = new Vector3(0,0,0);
+
+    Camera[] m_camera;
     // メニューのWindowにEditorExという項目を追加。
     [MenuItem("Window/HondyEditor/test")]
     static void Open()
@@ -23,36 +27,99 @@ public class TestRadarMeshTexture
 
     void OnGUI()
     {
-        EditorGUILayout.LabelField("hoge", GUI.skin.box);
-        
-        
-        m_rt = EditorGUILayout.ObjectField("RenderTexture", m_rt, typeof(RenderTexture), true) as RenderTexture;
 
-        if (GUILayout.Button("hoge！", GUI.skin.button))
+        m_mapCenter = EditorGUILayout.Vector3Field("マップの中心", m_mapCenter);
+     
+        if (GUILayout.Button("Set Render Texture", GUI.skin.button))
+        {
+            CreateRenderTexture();
+            SetCamera();
+            Debug.Log("Set Render Texture complete");
+
+        }
+        if (GUILayout.Button("Generate Map Texture", GUI.skin.button))
         {
             GenerateRenderRadarMeshTexture();
-            Debug.Log("GenerateRenderRadarMeshTexture");
+            AssetDatabase.Refresh(ImportAssetOptions.Default);
+            Debug.Log("GenerateRenderRadarMeshTexture Complete");
+
         }
 
-
-
+//         if (0 < _rt.Length)
+//         {
+//             if (_rt[0])
+//             {
+//                 m_tex2d = new Texture2D[9];
+//                 byte[] readBinary = File.ReadAllBytes("Assets/Hondy/" + _rt[0].name + ".png");
+//                 m_tex2d[0].LoadImage(readBinary); 
+//                 EditorGUI.DrawPreviewTexture(new Rect(0, 140, 128, 128), m_tex2d[0]);
+//             }
+//         }
 
     }
+    void SetCamera()
+    {
 
+        GameObject _parent = new GameObject();
+        GameObject _origin = new GameObject();
+
+        Camera _camera = _origin.AddComponent<Camera>();
+        _camera.orthographic = true;
+        _camera.orthographicSize = 15;
+        _camera.transform.Rotate(  (new Vector3( 90, 0, 0)));
+        GameObject[] objectArray = new GameObject[9];
+        for (int j = 0;j < 9;j++)
+        {
+            objectArray[j] = Instantiate(_origin) as GameObject;
+        }
+        m_camera = new Camera[9];
+        for (int i = 0; i < 9; i++)
+        {
+            m_camera[i] = objectArray[i].GetComponent<Camera>();
+            objectArray[i].transform.parent = _parent.transform;
+            Vector3 pos;
+            pos = m_mapCenter + new Vector3(i / 3 * 30, 0, i % 3 * 30);
+            objectArray[i].transform.position = pos;
+            RenderTexture rt = AssetDatabase.LoadAssetAtPath("Assets/Hondy/" + _rt[i].name + ".asset", typeof(Object)) as RenderTexture;
+            m_camera[i].targetTexture = rt;
+        }
+        GameObject.DestroyImmediate(_origin);
+    }
+
+    void CreateRenderTexture()
+    {
+
+        _rt = new RenderTexture[9];
+        for (int i = 0; i < 9;i++)
+        {
+            _rt[i] = new RenderTexture(512, 512,1000, RenderTextureFormat.Default);
+            _rt[i].name = "mapTexture" + i.ToString();
+            AssetDatabase.CreateAsset(_rt[i], "Assets/Hondy/" + _rt[i].name + ".asset");
+        }
+    }
 
     void GenerateRenderRadarMeshTexture()
     {
+        for (int i = 0; i < 9;i++)
+        {
 
-        Texture2D tex = new Texture2D(m_rt.width, m_rt.height, TextureFormat.RGB24, false);
-        RenderTexture.active = m_rt;
-        tex.ReadPixels(new Rect(0, 0, m_rt.width, m_rt.height), 0, 0);
-        tex.Apply();
+            Texture2D tex = new Texture2D(_rt[i].width, _rt[i].height, TextureFormat.RGB24, false);
+            RenderTexture.active = _rt[i];
+            tex.ReadPixels(new Rect(0, 0, _rt[i].width, _rt[i].height), 0, 0);
+            tex.Apply();
 
-        // Encode texture into PNG
-        byte[] bytes = tex.EncodeToPNG();
-        Object.DestroyImmediate(tex);
-        //Write to a file in the project folder
-        File.WriteAllBytes("/Assets/Hondy/" + m_rt.name + ".png", bytes);
+            // Encode texture into PNG
+            byte[] bytes = tex.EncodeToPNG();
+            Object.DestroyImmediate(tex);
+            //Write to a file in the project folder
+            File.WriteAllBytes("Assets/Hondy/" + _rt[i].name + ".png", bytes);
+        //    AssetDatabase.DeleteAsset("Assets/Hondy/" + _rt[i].name + ".asset");
+        }
+
+    }
+
+    void GenerateMapMesh()
+    {
 
     }
 }
